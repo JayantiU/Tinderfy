a0a4ad69affd7a9ba6a89b46ce06587f77a69068
tahjeirac <tahjeirac@users.noreply.github.com>
2020-07-25 17:24:18 -0400

    js version

--- untitled/TEST.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-
-results = sp.search(q='weezer', limit=20)
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
