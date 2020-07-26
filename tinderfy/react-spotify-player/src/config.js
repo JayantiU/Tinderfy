08bcfa3727435746ca0dde0cb5ede4d4bcd52eb5
JayantiU <33074861+JayantiU@users.noreply.github.com>
2020-07-26 00:26:44 -0400

    Using react to play and show music

---
+++ tinderfy/react-spotify-player/src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "4e05f24419024551923cfbbaa3363093";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
